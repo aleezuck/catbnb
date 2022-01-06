62f53489031b07384714b5e67a2a528a22f74a34
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-06 18:35:58 -0500

    Added index, new, create methods for Flats.

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,14 +1,26 @@
 class FlatsController < ApplicationController
   def index
+    @flats = policy_scope(Flat)
   end
 
   def show
   end
 
   def new
+    @flat = Flat.new
+    authorize(@flat)
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    authorize(@flat)
+
+    if @flat.save
+      redirect_to flats_path, notice: "Flat added successfully."
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +30,11 @@ class FlatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :location, :price, :description)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,7 @@
 class Flat < ApplicationRecord
   belongs_to :user
+
+  validates :title, :location, presence: true
+  validates :description, length: { minimum: 10 }
+  validates :price, numericality: { only_integer: true }
 end
