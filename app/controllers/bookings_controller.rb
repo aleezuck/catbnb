174a45009b99d87df29147d35bacc37b8e390343
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-07 16:21:02 -0500

    Added create method for bookings, with form on flat page.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,20 @@
 class BookingsController < ApplicationController
   def create
+    @booking = Booking.new(booking_params)
+    @booking.flat = Flat.find(params[:flat_id])
+    @booking.user = current_user
+    authorize(@booking)
+
+    if @booking.save
+      redirect_to flat_path(@booking.flat), notice: "Booking successful."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
