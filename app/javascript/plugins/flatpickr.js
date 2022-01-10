821905f50cccd2df29d9ec938a81ceaa6963e2c3
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-10 13:07:20 -0500

    Remove unavailable dates from booking form.

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,11 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
+  const unavailableDates = JSON.parse(document.querySelector('#book-flat').dataset.unavailable);
+
   flatpickr(".datepicker", {
+    minDate: "today",
+    disable: unavailableDates,
     altInput: true,
     allowInput: true
   });
