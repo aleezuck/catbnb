2a8bcfcddbb6ea452c7d03d97e4ac04361c71f4f
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-18 10:30:12 -0500

    Javascript to update price table after booking dates are selected.

--- app/javascript/controllers/booking_form_controller.js
+++ app/javascript/controllers/booking_form_controller.js
@@ -1,11 +1,24 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  connect() {
-    console.log('hello');
-  }
-
   updatePriceTable() {
-    console.log(this.element);
+    const startDate = document.getElementById('booking_start_date').value;
+    const endDate = document.getElementById('booking_end_date').value;
+    const bookingTable = document.getElementById('booking-price-table');
+    const bookingDays = document.getElementById('booking-days');
+    const bookingPrice = document.getElementById('booking-price');
+    const bookingSubmit = document.getElementById('booking-submit');
+
+    if (startDate && endDate) {
+      const checkIn = new Date(startDate);
+      const checkOut = new Date(endDate);
+      const days = (checkOut.getTime() - checkIn.getTime()) / (1000*60*60*24);
+      const price = document.getElementById('book-flat').dataset.price;
+      
+      bookingTable.classList.remove('d-none');
+      bookingSubmit.classList.remove('disabled');
+      bookingDays.innerHTML = days;
+      bookingPrice.innerHTML = `$${days * price}`;
+    }
   }
 }
