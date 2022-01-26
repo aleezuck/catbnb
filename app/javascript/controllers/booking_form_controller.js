1b59a85b81782b4e4469ee5c07ff1e7a82c0048b
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-25 23:13:26 -0500

    Refactored booking price table javascript using Stimulus.

--- app/javascript/controllers/booking_form_controller.js
+++ app/javascript/controllers/booking_form_controller.js
@@ -10,7 +10,7 @@ const disableBookingForm = () => {
 }
 
 export default class extends Controller {
-  static targets = ["startDate", "endDate", "bookFlat"]
+  static targets = ["startDate", "endDate", "bookFlat", "bookingPriceTable", "bookingSubmit", "bookingDays", "bookingPrice" ]
 
   connect() {
     const unavailableDates = JSON.parse(this.bookFlatTarget.dataset.unavailable);
@@ -58,12 +58,8 @@ export default class extends Controller {
   }
 
   updatePriceTable() {
-    const startDate = document.getElementById('booking_start_date').value;
-    const endDate = document.getElementById('booking_end_date').value;
-    const bookingTable = document.getElementById('booking-price-table');
-    const bookingDays = document.getElementById('booking-days');
-    const bookingPrice = document.getElementById('booking-price');
-    const bookingSubmit = document.getElementById('booking-submit');
+    const startDate = this.startDateTarget.value;
+    const endDate = this.endDateTarget.value;
 
     if (startDate && endDate) {
       const checkIn = new Date(startDate);
@@ -71,10 +67,10 @@ export default class extends Controller {
       const days = (checkOut.getTime() - checkIn.getTime()) / (1000*60*60*24);
       const price = document.getElementById('book-flat').dataset.price;
       
-      bookingTable.classList.remove('d-none');
-      bookingSubmit.disabled = false;
-      bookingDays.innerHTML = days;
-      bookingPrice.innerHTML = `$${days * price}`;
+      this.bookingPriceTableTarget.classList.remove('d-none');
+      this.bookingSubmitTarget.disabled = false;
+      this.bookingDaysTarget.innerHTML = days;
+      this.bookingPriceTarget.innerHTML = `$${days * price}`;
     }
   }
 }
